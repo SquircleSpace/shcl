7b3956c0b0b346694567ea861f7f6fd52af6549d
Brad Jensen <brad@bradjensen.net>
2016-07-03 16:23:25 -0700

    More debugging output

--- main.lisp
+++ main.lisp
@@ -2,7 +2,34 @@
 
 (optimization-settings)
 
+#+sbcl
+(defclass echo (sb-gray:fundamental-character-output-stream)
+    ())
+
+#+sbcl
+(defmethod sb-gray:stream-write-char ((s echo) char)
+  (format *standard-output* "CHAR: ~W~%" char)
+  char)
+
+#+sbcl
+(defparameter *echo-characters* nil)
+
+(defun debug-char-stream (stream)
+  (let* ((sbcl #+sbcl *echo-characters*))
+    (if sbcl
+        (make-echo-stream stream (make-instance 'echo))
+        stream)))
+
+(defun debug-token-iterator (stream)
+  (map-iterator (token-iterator stream)
+                (lambda (x)
+                  (format *standard-output* "TOKEN: ~A~%" x)
+                  x)
+                :type 'token-iterator))
+
 (defun main ()
-  (let ((command-iterator (command-iterator (token-iterator *standard-input*))))
-    (do-iterator (tree command-iterator)
-      (format *standard-output* "~A~%" tree))))
+  (let* ((stream (debug-char-stream *standard-input*))
+         (tokens (debug-token-iterator stream))
+         (commands (command-iterator tokens)))
+    (do-iterator (tree commands)
+      (format *standard-output* "TREE: ~A~%" tree))))
