7de26a030e093445ee276c7633f338bd0ba531de
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2016-10-04 18:12:42 -0700

    Almost support building on ECL
    ECL has a bug that prevents this from building.  When you build a
    monolithic library, ECL doesn't properly link in the dependencies.  The
    resulting .a contains .o files (for the parts of SHCL) and .a files (for
    the libraries that SHCL depends on).  This is malformed and results in
    errors at link time.
    
    I'm not TOO concerned about supporting ECL, since it has a few fatal
    flaws.
    1. SHCL takes forever to boot (I worked around the issue above).  I
    think that the dependencies that SHCL pulls in results in very long load
    times.  This is pretty unavoidable.
    2. ECL listens to SIGCHLD and automatically reaps all children.  This
    makes it incompatible with SHCL's need to wait for children.  It looks
    like this might be configurable (as an ECL boot option), but it is
    unclear whether that option actually changes anything.
    
    Mostly, building and (maybe, one day) testing under ECL is a good way to
    prove that SHCL is portable.  So, don't expect to run SHCL on ECL any
    time soon.

--- build-shcl.lisp
+++ build-shcl.lisp
@@ -2,12 +2,26 @@
 
 (let ((here (truename ".")))
   (push here asdf:*central-registry*))
+
 (handler-bind
     ((error
       (lambda (c)
         (format *error-output* "Fatal error: ~A~%" c)
         (uiop:quit 1))))
-  (asdf:compile-system :shcl)
-  (asdf:load-system :shcl)
-  (funcall (intern "OBSERVE-DUMP" (find-package "SHCL.UTILITY")))
-  (sb-ext:save-lisp-and-die "shcl" :toplevel (intern "MAIN" (find-package "SHCL")) :executable t :save-runtime-options t :purify t))
+  #-ecl
+  (progn
+    (asdf:compile-system :shcl)
+    (asdf:load-system :shcl)
+    (funcall (intern "OBSERVE-DUMP" (find-package "SHCL.UTILITY")))
+    #+sbcl (sb-ext:save-lisp-and-die "shcl" :toplevel (intern "MAIN" (find-package "SHCL")) :executable t :save-runtime-options t :purify t))
+  #+ecl
+  (progn
+    (asdf:register-immutable-system :asdf)
+    (asdf:register-immutable-system :uiop)
+
+    (defmethod asdf:output-files ((op asdf:monolithic-lib-op) (sys (eql (asdf:find-system "shcl"))))
+      (values (list "libshcl.a") t))
+
+    (asdf:operate
+     'asdf:monolithic-lib-op "shcl"))
+  (uiop:quit 0))
