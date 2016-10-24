814f3019a12cd41e694cf5e78252f6683b596a58
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2016-10-23 19:29:38 -0700

    Add cffi bindings for read

--- posix-types.lisp
+++ posix-types.lisp
@@ -4,4 +4,5 @@
    #:size-t #:ssize-t #:pid-t #:posix-spawn-file-actions-t
    #:posix-spawnattr-t #:dirent #:d-name #:errno #:mode-t #:environ
    #:s-irusr #:s-iwusr #:s-irgrp #:s-iroth #:o-rdonly #:o-wronly
-   #:o-rdwr #:o-creat #:o-trunc #:o-append #:f-getfd #:wuntraced))
+   #:o-rdwr #:o-creat #:o-trunc #:o-append #:f-getfd #:wuntraced
+   #:eintr))
