fd5fb418b033a912ccb515d10e10b026f02b5d1a
Brad Jensen <brad@bradjensen.net>
2016-11-10 17:36:25 -0800

    Compile output is too verbose!

--- make.lisp
+++ make.lisp
@@ -3,14 +3,47 @@
 (let ((here (truename ".")))
   (push here asdf:*central-registry*))
 
+#+sbcl
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (asdf:load-system :trivial-gray-streams))
+
+#+sbcl
+(progn
+  (defclass filtered (trivial-gray-streams:fundamental-character-output-stream)
+    ((line-buffer
+      :initform (make-string-output-stream))
+     (real-output
+      :initarg :output)))
+  (defmethod trivial-gray-streams:stream-write-char ((stream filtered) char)
+    (write-char char (slot-value stream 'line-buffer))
+    (unless (or (equal char #\return)
+                (equal char #\linefeed))
+      (return-from trivial-gray-streams:stream-write-char))
+
+    (let ((line (get-output-stream-string (slot-value stream 'line-buffer)))
+          (sign "; compiling"))
+      (unless (and (>= (length line) (length sign))
+                   (equal (subseq line 0 (length sign)) sign))
+        (write-string line (slot-value stream 'real-output))))))
+
+#+sbcl
+(defmacro reduce-compile-noise (&body body)
+  `(let ((*standard-output* (make-instance 'filtered :output *standard-output*)))
+     ,@body))
+
+#-sbcl
+(defmacro reduce-compile-noise (&body body)
+  `(progn ,@body))
+
 (handler-bind
     ((error
       (lambda (c)
-        (format *error-output* "Fatal error: ~A~%" c)
+        (format *error-output* "~%Fatal error: ~A~%" c)
         (uiop:quit 1))))
   #-ecl
   (progn
-    (asdf:compile-system :shcl)
+    (reduce-compile-noise
+      (asdf:compile-system :shcl))
     (asdf:load-system :shcl)
     (funcall (intern "OBSERVE-DUMP" (find-package "SHCL/UTILITY")))
     #+sbcl (sb-ext:save-lisp-and-die "shcl" :toplevel (intern "MAIN" (find-package "SHCL/MAIN")) :executable t :save-runtime-options t :purify t))
