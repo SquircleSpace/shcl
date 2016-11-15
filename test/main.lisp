f84639deca481c8318bf5c236abd022a2a01a27c
Brad Jensen <brad@bradjensen.net>
2016-11-14 18:48:18 -0800

    Add some tests for for loops

--- test/main.lisp
+++ test/main.lisp
@@ -6,18 +6,55 @@
 
 (define-builtin testing-assert-equal (args)
   (let ((set (fset:convert 'fset:set (fset:less-first args))))
-    (unless (equal 1 (fset:size set))
-      (error "Non-equal arguments: ~A" (fset:convert 'list set))))
-  (prove:pass "Strings matched")
-  0)
+    (if (equal 1 (fset:size set))
+        (progn
+          (pass "Strings matched")
+          0)
+        (progn
+          (fail (format nil "Strings didn't match ~A" set))
+          1))))
+
+(define-builtin testing-assert-unequal (args)
+  (let ((set (fset:convert 'fset:set (fset:less-first args))))
+    (if (equal (fset:size (fset:less-first args)) (fset:size set))
+        (progn
+          (pass "Strings didn't match")
+          0)
+        (progn
+          (fail (format nil "Strings match ~A" args))
+          1))))
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (require :sb-sprof))
 
 (deftest main
   (labels
-      ((run (s) (ok (exit-info-true-p
-                     (run-shell-commands-in-string s))
-                    (format nil "Command exited with code 0: ~A" s))))
-    (run "testing-assert-equal 0 0")
-    (run "FOO=123 ; testing-assert-equal $FOO 123")))
+      ((run (s &optional description)
+         (ok (exit-info-true-p
+              (run-shell-commands-in-string s))
+             (or description (format nil "Command exited with code 0: ~A" s)))))
+    (run "testing-assert-equal 0 0"
+         "Equality comparison works")
+    (run "testing-assert-unequal 1 0"
+         "Inequality comparison works")
+
+    (run "FOO=123 ; testing-assert-equal $FOO 123"
+         "Variable expansion works")
+
+    (run "testing-assert-equal '' \"$(for VAR in ; do
+echo $VAR
+done)\""
+         "For loops over empty lists work")
+    (run "testing-assert-equal '1
+2
+3
+' \"$(for VAR in 1 2 3
+do
+echo $VAR
+done)\""
+         "For loops over non-empty lists work")
+    (run "testing-assert-unequal $(for VAR in 1 2 3
+do
+echo $VAR
+done)"
+         "For loops over non-empty lists work")))
