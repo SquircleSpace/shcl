b242e2080494438b1a49e95386d49bac95df9f75
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2016-11-24 15:57:36 -0800

    Re-kajigger data classes a bit
    All data classes now inherit from data, and the standard methods are
    specialized on data instead of the subclasses.

--- core/data.lisp
+++ core/data.lisp
@@ -49,15 +49,10 @@
                  ,,set-var)
               `(,',access-macro ,,inner-clone))))))))
 
-(defmacro clone-slots (slots object maker)
-  (let ((new (gensym "NEW"))
-        (old (gensym "OLD"))
-        (slot (gensym "SLOT")))
-    `(let ((,new ,maker)
-           (,old ,object))
-       (dolist (,slot ',slots)
-         (setf (slot-value ,new ,slot) (slot-value ,old ,slot)))
-       ,new)))
+(defun clone-slots (slots old new)
+  (dolist (slot slots)
+    (setf (slot-value new slot) (slot-value old slot)))
+  new)
 
 (defgeneric clone (object))
 (defmethod clone (object)
@@ -66,8 +61,74 @@
 (defclass data-class (standard-class)
   ())
 
+(defclass data ()
+  ())
+
+(defmethod clone ((object data))
+  (clone-slots (mapcar 'closer-mop:slot-definition-name (closer-mop:class-slots (class-of object)))
+               object
+               (make-instance (class-of object))))
+
+(defmethod fset:compare ((first data) (second data))
+  (labels
+      ((slot-names (slots)
+         (mapcar 'closer-mop:slot-definition-name slots))
+       (compare-slots (slots)
+         (let ((result :equal))
+           (dolist (slot slots)
+             (ecase (fset:compare (slot-value first slot) (slot-value second slot))
+               (:less
+                (return-from compare-slots :less))
+               (:greater
+                (return-from compare-slots :greater))
+               (:unequal
+                (setf result :unequal))
+               (:equal)))
+           result)))
+    (declare (dynamic-extent #'compare-slots #'slot-names))
+
+    ;; Same class?  We can compare them!
+    (when (eq (class-of first) (class-of second))
+      (return-from fset:compare
+        (compare-slots (slot-names (closer-mop:class-slots (class-of first))))))
+
+    ;; Subtype?  Let's compare what we can!  Subtypes are greater than
+    ;; supertypes
+    (when (typep second (type-of first))
+      (let ((result (compare-slots (slot-names (closer-mop:class-slots (class-of first))))))
+        (return-from fset:compare
+          (if (eq :equal result)
+              :less
+              result))))
+
+    ;; Same as above
+    (when (typep first (type-of second))
+      (let ((result (compare-slots (slot-names (closer-mop:class-slots (class-of second))))))
+        (return-from fset:compare
+          (if (eq :equal result)
+              :greater
+              result))))
+
+    ;; Worst case.  Let's compare the slots that we can.  They might
+    ;; have some common ones.
+    (let* ((first-slots (slot-names (closer-mop:class-slots (class-of first))))
+           (second-slots (slot-names (closer-mop:class-slots (class-of second))))
+           (common-slots (intersection first-slots second-slots))
+           (result (compare-slots common-slots)))
+      (return-from fset:compare
+        (if (eq :equal result)
+            :unequal
+            result)))))
+
+(defmethod make-load-form ((object data) &optional environment)
+  (make-load-form-saving-slots
+   object
+   :slot-names (mapcar 'closer-mop:slot-definition-name (closer-mop:class-slots (class-of object)))
+   :environment environment))
+
 (defmethod closer-mop:validate-superclass ((data-class data-class) superclass)
-  (if (eq superclass (find-class 'standard-object))
+  (if (or (eq superclass (find-class 'standard-object))
+          (eq superclass (find-class 'data)))
       t
       (call-next-method)))
 
@@ -77,48 +138,9 @@
     :initform nil)))
 
 (defmethod closer-mop:finalize-inheritance :after ((class data-class))
-  (let ((slot-names (mapcar 'closer-mop:slot-definition-name
-                            (closer-mop:class-slots class))))
-    (eval
-     `(defmethod clone ((object ,(class-name class)))
-          (clone-slots ,slot-names
-           object
-           (make-instance ',(class-name class)))))
-
-    (eval
-     `(defmethod fset:compare ((first ,(class-name class)) (second ,(class-name class)))
-          (let ((result (fset:compare-slots
-                         first second
-                         ,@(mapcar (lambda (s) `',s) slot-names))))
-            (cond
-              ;; If they are :equal, then we need to make sure we
-              ;; considered all the slots.
-              ((not (eq :equal result)))
-
-              ;; If they are the same class, then we considered all the
-              ;; slots.  We don't have to worry about subclasses because
-              ;; subclasses would have their own method on compare.
-              ((eq (class-of first) (class-of second))
-               (assert (eq (find-class ',(class-name class)) (class-of first))))
-
-              ;; If second is a derived type, then first is less
-              ((typep second (type-of first))
-               (setf result :less))
-
-              ;; If first is a derived type, then first is greater
-              ((typep first (type-of second))
-               (setf result :greater))
-
-              ;; They are both derived types!  We've compared all their
-              ;; common slots and didn't find an order, so they are
-              ;; unequal.
-              (t
-               (setf result :unequal)))
-            result)))
-
-    (eval
-     `(defmethod make-load-form ((object ,(class-name class)) &optional environment)
-          (make-load-form-saving-slots object :slot-names ',slot-names :environment environment)))))
+  (let* ((superclasses (closer-mop:class-precedence-list class)))
+    (unless (member (find-class 'data) superclasses)
+      (error "data classes must inherit from data"))))
 
 (defmethod initialize-instance :around ((slot direct-data-slot) &rest initargs)
   (declare (ignore initargs))
@@ -144,6 +166,8 @@
 (defmacro define-data (name direct-superclasses direct-slots &rest options)
   (when (find :metaclass options :key #'car)
     (error "metaclass option is forbidden"))
+  (unless direct-superclasses
+    (setf direct-superclasses '(data)))
   ;; We need to eval the class definition now so that the updater
   ;; functions are generated.
   `(eval-when (:compile-toplevel :load-toplevel :execute)
