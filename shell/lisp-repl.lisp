9b2490f77ef8cabd2ec09f3e1e0930b269dc8eb9
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2017-01-14 15:16:25 -0800

    Clean up the lisp repl a bit
    This doesn't do much, now.  It will make future changes to support
    editline-powered input a bit easier.

--- shell/lisp-repl.lisp
+++ shell/lisp-repl.lisp
@@ -1,7 +1,9 @@
 (defpackage :shcl/shell/lisp-repl
-  (:use :common-lisp :shcl/core/builtin)
+  (:use :common-lisp :shcl/core/builtin :shcl/core/utility)
   (:export #:return-to-shell #:shell-help))
 (in-package :shcl/shell/lisp-repl)
+
+(optimization-settings)
 
 (defpackage :shcl-user
   (:use :common-lisp :shcl/shell/lisp-repl))
@@ -17,39 +19,44 @@
 :shell to return to SHCL
 "))
 
+(defun shcl-repl-read (stdin)
+  (let* ((eof-value '#:eof-value)
+         (form (read stdin nil eof-value)))
+    (when (eq form eof-value)
+      (debug-log status "SHCL-REPL EOF")
+      (format *standard-output* "~%")
+      (return-to-shell))
+
+    (debug-log status "SHCL-REPL READ ~A" form)
+    form))
+
+(defun print-list (list stdout)
+  (dolist (value list)
+    (format stdout "~A~%" value))
+  (finish-output stdout))
+
 (define-builtin shcl-repl (args)
   (declare (ignore args))
   (catch 'return-to-shell
-    (let ((eof-value '#:eof-value)
-          (*package* (find-package :shcl-user)))
-      (labels
-          ((print-list (list)
-             (dolist (value list)
-               (format *standard-output* "~A~%" value))
-             (finish-output *standard-output*))
-           (our-read ()
-             (let ((form (read *standard-input* nil eof-value)))
-               (when (eq form eof-value)
-                 (format *standard-output* "~%")
-                 (return-to-shell))
-
-               (case form
-                 (:shell
-                  (return-to-shell))
-                 (:help
-                  (shell-help)
-                  '(values))
-                 (otherwise
-                  form))))
-           (rep ()
-             (fresh-line *standard-output*)
-             (format *standard-output* "shcl (lisp)> ")
-             (finish-output *standard-output*)
-             (print-list (multiple-value-list (eval (our-read))))))
-        (loop
-           (restart-case (rep)
-             (restart-lisp-repl ()
-               (fresh-line *standard-output*))
-             (exit-lisp-repl ()
-               (return-to-shell)))))))
+    (let ((*package* (find-package :shcl-user)))
+      (loop
+         (restart-case
+             (progn
+               (fresh-line *standard-output*)
+               (format *standard-output* "shcl (lisp)> ")
+               (finish-output *standard-output*)
+               (let ((form (shcl-repl-read *standard-input*)))
+                 (case form
+                   (:shell
+                    (return-to-shell))
+                   (:help
+                    (shell-help)
+                    (values))
+                   (otherwise
+                    (print-list (multiple-value-list (eval form))
+                                *standard-output*)))))
+           (restart-lisp-repl ()
+             (fresh-line *standard-output*))
+           (exit-lisp-repl ()
+             (return-to-shell))))))
   0)
