27fcc3741c5f76e1c37c7bfce3fe84dc5c85697b
Brad Jensen <brad@bradjensen.net>
2017-01-16 14:59:11 -0800

    Document shcl/shell/main

--- shell/main.lisp
+++ shell/main.lisp
@@ -15,16 +15,34 @@
 
 (optimization-settings)
 
-(defparameter *shell-readtable* +standard-shell-readtable+)
+(defparameter *shell-readtable* +standard-shell-readtable+
+  "The shell readtable used by the main shell loop.
 
-(defparameter *fresh-prompt* t)
+This only changes how `shcl/shell/main:main' reads shell commands.
+Macros and functions which consume shell expressions are not impacted
+in any way by this variable.")
+
+(defparameter *fresh-prompt* t
+  "If non-nil, then the next time the user is prompted for input, the
+standard prompt should be displayed.
+
+When nil, a prompt indicating that the previous line is incomplete
+should be displayed.")
 
 (defun main-prompt ()
+  "Return the string which should be displayed as the prompt for the
+next line.
+
+See `*fresh-prompt*'."
   (let ((result (if *fresh-prompt* "shcl> " "> ")))
     (setf *fresh-prompt* nil)
     result))
 
 (defun main-token-iterator (stream form-queue)
+  "Return an iterator that produces the tokens found in `stream'.
+
+Tokens are read using `*shell-readtable*', and their bake forms (see
+`shcl/core/baking:bake-tokens') are added to form-queue."
   (lookahead-iterator-wrapper
    (bake-tokens
     (make-iterator ()
@@ -36,6 +54,11 @@
     form-queue)))
 
 (defun restartable-command-iterator (stream form-queue)
+  "Return an iterator that emits the fully parsed commands that
+`stream' contains.
+
+Tokens are read using `main-token-iterator', and bake forms are placed
+in `form-queue'.  See `shcl/core/baking:bake-tokens'."
   (let* ((tokens (main-token-iterator stream form-queue))
          (commands (command-iterator tokens)))
     (labels
@@ -55,16 +78,28 @@
                (reset-token-iterator)
                (go start))))))))
 
-(defparameter *enable-lisp-splice* nil)
+(defparameter *enable-lisp-splice* nil
+  "SHCL should permit splicing in lisp forms.
+
+Do not set this variable directly.  `main' will set this based on the
+arguments it receives.")
 (defparameter *debug* nil)
-(defparameter *help* nil)
+(defparameter *help* nil
+  "SHCL should display some basic help information and then exit.
+
+Do not set this variable directly.  `main' will set this based on the
+arguments it receives.")
 
 (defparameter *options*
   '((*enable-lisp-splice* nil "Extend shell language to support splicing lisp expressions")
     (*debug* nil "Run a SWANK server")
-    (*help* nil "Show list of options")))
+    (*help* nil "Show list of options"))
+  "The set of options recognized by `main'.  See `cl-cli:parse-cli'.")
 
 (defmacro with-options ((argv &key (options '*options*)) &body body)
+  "Parse options and establish dynamic bindings for them.
+
+See `cl-cli:parse-cli'."
   (let ((vars (gensym "VARS"))
         (values (gensym "VALUES"))
         (command (gensym "COMMAND"))
@@ -76,6 +111,11 @@
          ,@body))))
 
 (defun run-shell-commands-in-stream (stream)
+  "Read and evaluate shell commands contained within the given
+stream.
+
+This function will not display prompts.  You probably want to provide
+a stream like the one created by `shcl/shell/prompt:make-editline-stream'."
   (let* ((form-queue (make-queue))
          (commands (restartable-command-iterator stream form-queue))
          (*fresh-prompt* t)
@@ -100,6 +140,7 @@
     last-result))
 
 (define-builtin enable-lisp-syntax (args)
+  "Permit the use of lisp splice forms in shell expressions."
   (unless (equal 1 (fset:size args))
     (return-from enable-lisp-syntax 1))
   (setf *shell-readtable* (use-table *shell-readtable* *splice-table*))
@@ -112,6 +153,14 @@
     (funcall (intern "INIT" (find-package "SWANK-LOADER")))))
 
 (defun main ()
+  "Start running SHCL's shell.
+
+This function assumes that SHCL \"owns\" the process.  That means that
+the process environment won't change without SHCL's knowledge and that
+SHCL has total control over the process environment.  This means, for
+example, that...
+* SHCL is soley responsible for all file descriptor manipulations
+* SHCL may install or remove any signal handlers it wishes"
   (observe-revival)
   (with-options ((uiop:raw-command-line-arguments))
     (when *help*
