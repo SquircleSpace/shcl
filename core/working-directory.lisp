fc6c6f9aad13f9cbe83864d916b34c118a15efc1
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2017-01-18 18:55:38 -0800

    Document shcl/core/working-directory

--- core/working-directory.lisp
+++ core/working-directory.lisp
@@ -12,39 +12,63 @@
 the process."
   (open-retained "." O-RDONLY))
 
-(defvar *working-directory-fds* nil)
-(defvar *working-directory-lock* (make-lock))
+(defvar *working-directory-fds* nil
+  "The current stack of working directories")
+(defvar *working-directory-lock* (make-lock)
+  "The lock protecting access to `*working-directory-fds*'.")
 (defun initialize-working-directory-fds ()
+  "Set `*working-directory-fds*' to its initial value.
+
+This should not be called directly.  It is intended to be called by
+`shcl/core/utility:observe-revival' when the process starts."
   (setf *working-directory-fds* (list (process-working-directory-retained))))
 (on-revival initialize-working-directory-fds)
 
 (defun %current-working-directory-fd ()
+  "The non-locking version of `current-working-directory-fd'."
   (or (first *working-directory-fds*)
       (fd-autorelease (process-working-directory-retained))))
 
 (defun current-working-directory-fd ()
+  "Return a fd representing the current working directory as seen by
+the current thread."
   (with-lock-held (*working-directory-lock*)
     (%current-working-directory-fd)))
 
 (defun release-history (history)
+  "Release the file descriptors in the given working directory history."
   (dolist (item history)
     (fd-release item)))
 
 (defun retain-history (history)
+  "Retain the file descriptors in the given working directory history."
   (dolist (item history)
     (fd-retain item)))
 
 (defun preserve-working-directory-history ()
+  "Create a copy of the current working directory history.
+
+This copy must be destroyed with
+`destroy-preserved-working-directory-history'."
   (with-lock-held (*working-directory-lock*)
     (let* ((fds *working-directory-fds*))
       (retain-history fds)
       fds)))
 
 (defun destroy-preserved-working-directory-history (history)
+  "Reclaim resources associated with the given working directory history.
+
+After calling this function, the given history must not be used
+again."
   (release-history history)
   (values))
 
 (defun call-with-alternate-working-directory-history (history fn)
+  "Call the given `fn' in a context where the working directory has been
+switched to `history'.
+
+The `history' argument must be a value returned by
+`preserve-working-directory-history'."
   (let ((*working-directory-lock* (make-lock))
         (*working-directory-fds* history))
     ;; The history might change once we start executing body.  So,
@@ -61,6 +85,9 @@ the process."
  'destroy-preserved-working-directory-history)
 
 (defun %try-opening-path (path)
+  "Open the given path if it exists and return the fd associated with the file.
+
+Returns nil if the given path doesn't exist."
   (handler-bind
       ((syscall-error
         (lambda (e)
@@ -69,6 +96,7 @@ the process."
     (fd-autorelease (openat-retained (%current-working-directory-fd) path o-rdonly))))
 
 (defun directory-p (path)
+  "Returns non-nil if the given path represents a directory."
   (with-lock-held (*working-directory-lock*)
     (with-fd-scope ()
       (let ((fd (%try-opening-path path)))
@@ -76,29 +104,42 @@ the process."
           (directory-fd-p fd))))))
 
 (defun directory-fd-p (fd)
+  "Returns non-nil if the given fd points at a directory."
   (s-isdir (slot-value (fstat fd) 'st-mode)))
+
+(defgeneric path-invalid-message (condition)
+  (:documentation
+   "Access the human-readable description of of what is wrong"))
 
 (define-condition path-invalid (error)
   ((message
     :type string
     :initarg :message
-    :reader path-invalid-message))
-  (:report (lambda (c s) (format s "Path is invalid.  ~A" (path-invalid-message c)))))
+    :reader path-invalid-message
+    :documentation
+    "The human-readable description of what is wrong"))
+  (:report (lambda (c s) (format s "Path is invalid.  ~A" (path-invalid-message c))))
+  (:documentation
+   "A condition that is signaled when a given path string isn't suitable."))
 
 (defun %pop-working-directory ()
+  "The non-locking version of `pop-working-directory'."
   (let ((fd (pop *working-directory-fds*)))
     (fd-release fd)
     (values)))
 
 (defun pop-working-directory ()
+  "Pop a directory off of the working directory history."
   (with-lock-held (*working-directory-lock*)
     (%pop-working-directory)))
 
 (defun %push-working-directory-fd (fd)
+  "The non-locking, fd-centric version of `push-working-directory'."
   (fd-retain fd)
   (push fd *working-directory-fds*))
 
 (defun push-working-directory (path)
+  "Push a new directory onto the working directory history."
   (with-lock-held (*working-directory-lock*)
     (with-fd-scope ()
       (let ((dir-fd (%try-opening-path path)))
@@ -107,6 +148,10 @@ the process."
         (%push-working-directory-fd dir-fd)))))
 
 (defun cd (path)
+  "Change the current working directory.
+
+This is more or less equivalent to calling `pop-working-directory' and
+then `push-working-directory'."
   (with-lock-held (*working-directory-lock*)
     (with-fd-scope ()
       (let ((dir-fd (%try-opening-path path)))
