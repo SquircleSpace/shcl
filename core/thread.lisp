3c3f9a23f8e44eb6f38a980169ad04d3518c0b2f
Brad Jensen <brad@bradjensen.net>
2017-02-07 17:57:50 -0800

    Document shcl/core/thread

--- core/thread.lisp
+++ core/thread.lisp
@@ -4,6 +4,7 @@
    ;; Semaphores
    #:semaphore #:make-semaphore #:semaphore-signal #:semaphore-wait
    #:semaphore-p
+   ;; Queues
    #:queue #:make-queue #:enqueue #:dequeue #:dequeue-no-block #:queue-p))
 (in-package :shcl/core/thread)
 
@@ -11,14 +12,19 @@
 
 (defstruct (semaphore
              (:constructor %make-semaphore))
+  "This struct represents the standard semaphore synchronization
+primitive."
   (count 0)
   (lock (make-lock))
   (cv (make-condition-variable)))
 
 (defun make-semaphore ()
+  "Create a new semaphore."
   (%make-semaphore))
 
 (defun semaphore-signal (semaphore)
+  "Increment the given semaphore and allow a thread blocked in
+`semaphore-wait' to continue."
   (with-accessors ((count semaphore-count)
                    (lock semaphore-lock) (cv semaphore-cv))
       semaphore
@@ -28,6 +34,8 @@
       nil)))
 
 (defun semaphore-wait (semaphore)
+  "Either decrement the given semaphore immediately or wait until the
+semaphore is incremented by `semaphore-signal'."
   (with-accessors ((count semaphore-count)
                    (lock semaphore-lock) (cv semaphore-cv))
       semaphore
@@ -39,17 +47,22 @@
 
 (defstruct (queue
              (:constructor %make-queue))
+  "This struct is a standard queue data structure."
   front
   back
   (lock (make-lock))
   (cv (make-condition-variable)))
 
 (defun make-queue ()
+  "Create a new queue."
   (%make-queue))
 
 (defun enqueue (item queue)
-  (with-accessors ((front queue-front) (back queue-back)
-                   (lock queue-lock) (cv queue-cv)) queue
+  "Add an item to the given queue."
+  (with-accessors
+        ((front queue-front) (back queue-back)
+         (lock queue-lock) (cv queue-cv))
+      queue
     (with-lock-held (lock)
       (cond
         ((null front)
@@ -63,8 +76,11 @@
       nil)))
 
 (defun %dequeue (queue &key (wait t))
-  (with-accessors ((front queue-front) (back queue-back)
-                   (lock queue-lock) (cv queue-cv)) queue
+  "The brains of `dequeue' and `dequeue-no-block'."
+  (with-accessors
+        ((front queue-front) (back queue-back)
+         (lock queue-lock) (cv queue-cv))
+      queue
     (with-lock-held (lock)
       (when (and (not wait) (null front))
         (return-from %dequeue (values nil nil)))
@@ -77,9 +93,17 @@
         (values item t)))))
 
 (defun dequeue (queue)
+  "Return and remove the item at the front of the queue.
+
+This function will block if there are no items in the queue.  See
+`dequeue-no-block'."
   (nth-value 0 (%dequeue queue)))
 
 (defun dequeue-no-block (queue &optional default)
+  "Return and remove the item at the front of the queue.
+
+This function will return immediately if there aren't any items in the
+queue."
   (multiple-value-bind (value valid) (%dequeue queue :wait nil)
     (if valid
         value
