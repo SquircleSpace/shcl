5e7207568dea932036f0f20c1282aec340904dd5
Ada Avery <2561504+SquircleSpace@users.noreply.github.com>
2019-06-18 19:00:13 -0700

    Remove iterators from shcl/shell/directory

--- shell/directory.lisp
+++ shell/directory.lisp
@@ -14,9 +14,12 @@
 
 (defpackage :shcl/shell/directory
   (:use
-   :common-lisp :cffi :shcl/core/utility :shcl/core/iterator :shcl/core/command
+   :common-lisp :cffi :shcl/core/utility :shcl/core/command
    :shcl/core/working-directory :shcl/core/fd-table
    :shcl/core/lisp-interpolation :shcl/core/posix :shcl/core/posix-types)
+  (:import-from :shcl/core/sequence
+   #:walk #:head #:tail #:empty-p #:do-sequence #:immutable-cons
+   #:lazy-sequence)
   (:import-from :shcl/core/environment #:env #:$pwd #:$oldpwd #:$home
                 #:$cdpath #:split-colon-list)
   (:export #:physical-pwd))
@@ -30,50 +33,52 @@ directory."
   (let ((raw-path (capture (:streams '(:stdout)) (evaluate-constant-shell-string "pwd -P"))))
     (string-right-trim #(#\newline) raw-path)))
 
-(defun path-iterator (path)
-  (let ((current-part (make-string-output-stream))
-        (string-iterator (vector-iterator path))
-        (first-p t)
-        (slash-count 0))
-    (make-computed-iterator
-      (labels
-          ((emit-slash ()
-             (cond
-               ((equal slash-count 2)
-                (setf slash-count nil)
-                (emit "//"))
-               ((not (zerop slash-count))
-                (setf slash-count nil)
-                (emit "/")))))
-        (do-iterator (c string-iterator)
-          (cond
-            (first-p
-             (case c
-               (#\/
-                (incf slash-count))
-               (otherwise
-                (setf first-p nil)
-                (write-char c current-part)
-                (emit-slash))))
+(defun %split-path (path first-p)
+  (when (zerop (length path))
+    (return-from %split-path nil))
 
-            (t
-             (case c
-               (#\/
-                (let ((part (get-output-stream-string current-part)))
-                  (unless (zerop (length part))
-                    (emit part))))
+  (labels
+      ((tail-from (index)
+         (cond
+           ((null index)
+            "")
+           ((zerop index)
+            path)
+           ((< index (length path))
+            (make-array (- (length path) index)
+                        :element-type (array-element-type path)
+                        :displaced-to path
+                        :displaced-index-offset index))))
+       (head-to (index)
+         (cond
+           ((and index (<= index (length path)))
+            (subseq path 0 index))
+           ((array-displacement path)
+            (copy-seq path))
+           (t
+            path)))
+       (split (head-stopping-point tail-starting-point)
+         (let ((tail-string (tail-from tail-starting-point)))
+           (immutable-cons (head-to head-stopping-point)
+                           (lazy-sequence
+                             (%split-path tail-string nil)))))
+       (slash (char)
+         (equal #\/ char)))
 
-               (otherwise
-                (write-char c current-part))))))
-        (when slash-count
-          (emit-slash))
-        (let ((part (get-output-stream-string current-part)))
-          (if (zerop (length part))
-              (stop)
-              (emit part)))))))
+    (let* ((first-slash-position (position-if #'slash path))
+           (after-slash-position (when first-slash-position
+                                   (or (position-if-not #'slash path :start (1+ first-slash-position))
+                                       (length path)))))
+      (when (and first-slash-position (zerop first-slash-position))
+        (assert first-p)
+        (let ((slash-count (- after-slash-position first-slash-position)))
+          (return-from %split-path
+            (split (if (equal 2 slash-count) 2 1) after-slash-position))))
 
-(defun path-parts (path)
-  (iterable-values (path-iterator path)))
+      (split first-slash-position after-slash-position))))
+
+(defun split-path (path)
+  (%split-path path t))
 
 (defun combine-path-parts (parts)
   (let ((result (make-string-output-stream)))
@@ -107,13 +112,13 @@ directory."
          (go step-7))
 
        ;; Step 4
-       (let ((first-part (next (path-iterator path))))
+       (let ((first-part (head (split-path path))))
          (when (or (equal "." first-part)
                    (equal ".." first-part))
            (go step-6)))
 
        ;; Step 5
-       (shcl/core/sequence:do-sequence (cdpath (split-colon-list $cdpath))
+       (do-sequence (cdpath (split-colon-list $cdpath))
          (let* ((cdpath (if (equal "" cdpath) "./" cdpath))
                 (slash-terminated (equal #\/ (aref cdpath (- (length cdpath) 1))))
                 (query-path (concatenate 'string cdpath (if slash-terminated "" "/") path)))
@@ -136,9 +141,9 @@ directory."
            (setf curpath (concatenate 'string pwd slash curpath))))
 
        ;; Step 8
-       (let ((parts (path-parts curpath))
+       (let ((parts (split-path curpath))
              (clean-parts (make-extensible-vector)))
-         (loop :for part :across parts :do
+         (do-sequence (part parts)
             (cond
               ((equal part ".")) ;; Do nothing
 
